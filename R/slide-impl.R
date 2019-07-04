b0698173df54e13205f959b4fb0cf3aab013d210
DavisVaughan <davis@rstudio.com>
2019-07-04 14:56:09 -0400

    Extract out `slide_impl()` and implement `slide_dbl()` and `slide_dfr()`

---
+++ R/slide-impl.R
@@ -0,0 +1,151 @@
+slide_impl <- function(.x,
+                       .f,
+                       ...,
+                       .size,
+                       .step,
+                       .align,
+                       .partial,
+                       .dir,
+                       .ptype) {
+
+  vec_assert(.x)
+  vec_assert(.size, size = 1L)
+  vec_assert(.step, size = 1L)
+  vec_assert(.align, character(), 1L)
+  vec_assert(.partial, logical(), 1L)
+  vec_assert(.dir, character(), 1L)
+
+  .size <- vec_cast(.size, integer())
+  .step <- vec_cast(.step, integer())
+
+  arg_match(.dir, valid_dir())
+  arg_match(.align, valid_align())
+
+  .f <- as_function(.f)
+  .x_n <- vec_size(.x)
+
+  if (.size > .x_n) {
+    glubort("The size of `.x` ({.x_n}) cannot be less than `.size` ({.size}).")
+  }
+
+  if (.size <= 0L) {
+    glubort("`.size` must be at least 1, not {.size}.")
+  }
+
+  if (.step <= 0L) {
+    glubort("`.step` must be at least 1, not {.step}.")
+  }
+
+  forward <- .dir == "forward"
+
+  out <- vec_init(.ptype, n = .x_n)
+
+  assign <- get_assigner(.ptype)
+
+  iterations_n <- iterations(.x_n, .size, .step, .align, .partial, forward)
+  complete_iterations_n <- iterations(.x_n, .size, .step, .align, FALSE, forward)
+
+  if (forward) {
+    start <- 1L
+    stop <- .size
+    entry <- .size - offset_align(.size, .align) + 1L
+    endpoint <- .x_n
+  }
+  else {
+    start <- .x_n
+    stop <- .x_n - .size + 1L
+    entry <- .x_n - offset_align(.size, .align) + 1L
+    endpoint <- 1L
+    .step <- -.step
+  }
+
+  .step_stop <- .step
+
+  for (j in seq_len(iterations_n)) {
+    i <- seq(from = start, to = stop)
+
+    elt <- .f(vec_slice(.x, i), ...)
+    out <- assign(out, entry, value = elt)
+
+    if (.partial && j == complete_iterations_n) {
+      stop <- endpoint # set stop to the endpoint value
+      .step_stop <- 0L # stop incrementing stop as we can no longer perform a full step
+      .partial <- FALSE # no need to check second condition anymore
+    }
+
+    start <- start + .step
+    stop <- stop + .step_stop
+    entry <- entry + .step
+  }
+
+  out
+}
+
+# ------------------------------------------------------------------------------
+
+# Need something that can assign elements of a list and of
+# a non recursive vector
+# https://github.com/r-lib/vctrs/issues/141
+get_assigner <- function(x) {
+  if (is.recursive(x)) {
+    `[[<-`
+  } else {
+    vec_assign
+  }
+}
+
+# ------------------------------------------------------------------------------
+
+valid_dir <- function() {
+  c("forward", "backward")
+}
+
+valid_align <- function() {
+  c("right", "left", "center", "center-left", "center-right")
+}
+
+# ------------------------------------------------------------------------------
+
+offset_align <- function(size, align) {
+  switch(
+    align,
+    "left" = size,
+    "right" = 1L,
+    "center" =,
+    "center-left" = ceiling(median2(size)),
+    "center-right" = floor(median2(size))
+  )
+}
+
+offset_dir <- function(forward, size, align) {
+  if (forward) {
+    offset_align(size, align)
+  } else {
+    size - offset_align(size, align) + 1L
+  }
+}
+
+offset <- function(partial, forward, size, align) {
+  if (partial) {
+    offset_dir(forward, size, align)
+  } else {
+    1L
+  }
+}
+
+iterations <- function(n, size, step, align, partial, forward) {
+  offset <- offset(partial, forward, size, align)
+  ceiling((n - size + offset) / step)
+}
+
+# ------------------------------------------------------------------------------
+
+# compute the median from a length
+# figured it out after looking at the definition of median on wiki
+# https://en.wikipedia.org/wiki/Median
+# mimics median(seq_len(n)) but is much faster
+# bench::mark(median(seq_len(5)), median2(5))
+median2 <- function(n) {
+  i <- (n + 1) / 2
+  (floor(i) + ceiling(i)) / 2
+}
--- R/slide.R
+++ R/slide.R
@@ -46,129 +46,67 @@ slide <- function(.x,
                   .align = "right",
                   .partial = FALSE,
                   .dir = "forward") {
-
-  vec_assert(.x)
-  vec_assert(.size, size = 1L)
-  vec_assert(.step, size = 1L)
-  vec_assert(.align, character(), 1L)
-  vec_assert(.partial, logical(), 1L)
-  vec_assert(.dir, character(), 1L)
-
-  .size <- vec_cast(.size, integer())
-  .step <- vec_cast(.step, integer())
-
-  arg_match(.dir, valid_dir())
-  arg_match(.align, valid_align())
-
-  .f <- as_function(.f)
-  .x_n <- vec_size(.x)
-
-  if (.size > .x_n) {
-    glubort("The size of `.x` ({.x_n}) cannot be less than `.size` ({.size}).")
-  }
-
-  if (.size <= 0L) {
-    glubort("`.size` must be at least 1, not {.size}.")
-  }
-
-  if (.step <= 0L) {
-    glubort("`.step` must be at least 1, not {.step}.")
-  }
-
-  forward <- .dir == "forward"
-
-  out <- new_list(.x_n)
-
-  iterations_n <- iterations(.x_n, .size, .step, .align, .partial, forward)
-  complete_iterations_n <- iterations(.x_n, .size, .step, .align, FALSE, forward)
-
-  if (forward) {
-    start <- 1L
-    stop <- .size
-    entry <- .size - offset_align(.size, .align) + 1L
-    endpoint <- .x_n
-  }
-  else {
-    start <- .x_n
-    stop <- .x_n - .size + 1L
-    entry <- .x_n - offset_align(.size, .align) + 1L
-    endpoint <- 1L
-    .step <- -.step
-  }
-
-  .step_stop <- .step
-
-  for (j in seq_len(iterations_n)) {
-    i <- seq(from = start, to = stop)
-
-    out[[entry]] <- .f(vec_slice(.x, i), ...)
-
-    if (.partial && j == complete_iterations_n) {
-      stop <- endpoint # set stop to the endpoint value
-      .step_stop <- 0L # stop incrementing stop as we can no longer perform a full step
-      .partial <- FALSE # no need to check second condition anymore
-    }
-
-    start <- start + .step
-    stop <- stop + .step_stop
-    entry <- entry + .step
-  }
-
-  out
-}
-
-# ------------------------------------------------------------------------------
-
-valid_dir <- function() {
-  c("forward", "backward")
-}
-
-valid_align <- function() {
-  c("right", "left", "center", "center-left", "center-right")
-}
-
-# ------------------------------------------------------------------------------
-
-offset_align <- function(size, align) {
-  switch(
-    align,
-    "left" = size,
-    "right" = 1L,
-    "center" =,
-    "center-left" = ceiling(median2(size)),
-    "center-right" = floor(median2(size))
+  slide_impl(
+    .x = .x,
+    .f = .f,
+    ...,
+    .size = .size,
+    .step = .step,
+    .align = .align,
+    .partial = .partial,
+    .dir = .dir,
+    .ptype = list()
   )
 }
 
-offset_dir <- function(forward, size, align) {
-  if (forward) {
-    offset_align(size, align)
-  } else {
-    size - offset_align(size, align) + 1L
-  }
+#' @rdname slide
+#' @export
+slide_dbl <- function(.x,
+                      .f,
+                      ...,
+                      .size = 1L,
+                      .step = 1L,
+                      .align = "right",
+                      .partial = FALSE,
+                      .dir = "forward") {
+  slide_impl(
+    .x = .x,
+    .f = .f,
+    ...,
+    .size = .size,
+    .step = .step,
+    .align = .align,
+    .partial = .partial,
+    .dir = .dir,
+    .ptype = double()
+  )
 }
 
-offset <- function(partial, forward, size, align) {
-  if (partial) {
-    offset_dir(forward, size, align)
-  } else {
-    1L
-  }
-}
+#' @rdname slide
+#' @export
+slide_dfr <- function(.x,
+                      .f,
+                      ...,
+                      .size = 1L,
+                      .step = 1L,
+                      .align = "right",
+                      .partial = FALSE,
+                      .dir = "forward",
+                      .names_to = NULL,
+                      .name_repair = c("unique", "universal", "check_unique")) {
+  out <- slide(
+    .x = .x,
+    .f = .f,
+    ...,
+    .size = .size,
+    .step = .step,
+    .align = .align,
+    .partial = .partial,
+    .dir = .dir
+  )
 
-iterations <- function(n, size, step, align, partial, forward) {
-  offset <- offset(partial, forward, size, align)
-  ceiling((n - size + offset) / step)
-}
+  # should there be a size constraint?
+  # lapply(out, vec_assert, size = 1)
 
-# ------------------------------------------------------------------------------
-
-# compute the median from a length
-# figured it out after looking at the definition of median on wiki
-# https://en.wikipedia.org/wiki/Median
-# mimics median(seq_len(n)) but is much faster
-# bench::mark(median(seq_len(5)), median2(5))
-median2 <- function(n) {
-  i <- (n + 1) / 2
-  (floor(i) + ceiling(i)) / 2
+  vec_rbind(!!!out, .names_to = .names_to, .name_repair = .name_repair)
 }
