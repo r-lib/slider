2bc5ef4fb91da58340d33ab935bf4b1f5053e9f9
DavisVaughan <davis@rstudio.com>
2019-07-06 15:18:38 -0400

    Add `tile()`

---
+++ R/tile.R
@@ -0,0 +1,23 @@
+tile <- function(.x,
+                 .f,
+                 ...,
+                 .before = 0L,
+                 .after = 0L,
+                 .offset = NULL,
+                 .partial = FALSE,
+                 .dir = "forward") {
+
+  .step <- .before + .after + 1L
+
+  slide(
+    .x = .x,
+    .f = .f,
+    ...,
+    .before = .before,
+    .after = .after,
+    .step = .step,
+    .offset = .offset,
+    .partial = .partial,
+    .dir = .dir
+  )
+}
