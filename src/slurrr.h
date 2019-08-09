5fafc72804b1efbbdae98c487db0b3e5a7c5edb0
DavisVaughan <davis@rstudio.com>
2019-08-09 10:55:14 -0400

    Move parameter logic to `param.c`

--- src/slurrr.h
+++ src/slurrr.h
@@ -7,4 +7,24 @@
 #include <Rinternals.h>
 #include <stdbool.h>
 
+// -----------------------------------------------------------------------------
+// Parameters
+
+struct slide_params {
+  int type;
+  int size;
+  bool constrain;
+  int before;
+  int after;
+  int step;
+  int offset;
+  bool complete;
+  bool forward;
+  bool before_unbounded;
+  bool after_unbounded;
+};
+
+// Defined in param.c
+struct slide_params init_params(SEXP x);
+
 #endif
