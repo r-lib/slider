606d38738515bbe999eaeee902d716e721b65fc6
DavisVaughan <davis@rstudio.com>
2019-08-19 19:15:51 -0400

    Massively simplify `slide_new()` to precompute the maximum number of iterations

--- R/slide-new.R
+++ R/slide-new.R
@@ -31,284 +31,138 @@ slide_new_impl <- function(.x,
   n <- vec_size(.x)
   .f <- as_function(.f)
 
-  params <- list(
-    before = .before,
-    after = .after,
-    before_unbounded = is_unbounded(.before),
-    after_unbounded = is_unbounded(.after),
-    step = .step,
-    complete = .complete,
-    ptype = .ptype,
-    constrain = .constrain,
-    entry = 1L,
-    position = 1L,
-    n = n
-  )
+  # check_params()
+  params <- init_params(.before, .after, .step, .complete, .constrain, .ptype, n)
 
-  params <- check_params(params)
   window_params <- init_window_params(params)
 
-  if (params$before_unbounded && params$after_unbounded) {
-    loop_double_unbounded_new(.x, .f, params, window_params, ...)
-  } else if (params$before_unbounded) {
-    loop_before_unbounded_new(.x, .f, params, window_params, ...)
-  } else if (params$after_unbounded) {
-    loop_after_unbounded_new(.x, .f, params, window_params, ...)
-  } else {
-    loop_bounded_new(.x, .f, params, window_params, ...)
+  out <- vec_init(.ptype, params$n)
+
+  while(params$iteration <= params$iteration_max) {
+    start <- max(window_params$start, 0L)
+    stop <- min(window_params$stop, params$n)
+
+    slice <- vec_slice(.x, seq2(start, stop))
+
+    elt <- .f(slice, ...)
+
+    if (params$constrain) {
+      elt <- vec_cast(elt, .ptype)
+
+      if (vec_size(elt) != 1L) {
+        abort(sprintf("The size of each result of `.f` must be size 1. Iteration %i was size %i.", params$iteration, vec_size(elt)))
+      }
+
+      out <- vec_assign(out, params$iteration, elt)
+    } else {
+      out[[params$iteration]] <- elt
+    }
+
+    params$iteration <- params$iteration + params$step
+    window_params$start <- window_params$start + window_params$start_step
+    window_params$stop <- window_params$stop + window_params$stop_step
   }
+
+  out
+}
+
+# ------------------------------------------------------------------------------
+
+init_params <- function(before, after, step, complete, constrain, ptype, n) {
+  before_unbounded <- is_unbounded(before)
+  after_unbounded <- is_unbounded(after)
+
+  if (before_unbounded) {
+    before <- 0L
+  }
+
+  if (after_unbounded) {
+    after <- 0L
+  }
+
+  before_positive <- before >= 0L
+  after_positive <- after >= 0L
+
+  if (!before_positive && !after_positive) {
+    abort("Both `.before` and `.after` cannot be negative.")
+  }
+
+  iteration <- 1L
+  iteration_max <- n
+
+  if (complete) {
+    if (before_positive) {
+      iteration <- iteration + before
+    }
+
+    if (after_positive) {
+      iteration_max <- iteration_max - after
+    }
+  } else {
+    if (!before_positive) {
+      iteration_max <- iteration_max - abs(before)
+    }
+
+    if (!after_positive) {
+      iteration <- iteration + abs(after)
+    }
+  }
+
+  list(
+    before = before,
+    after = after,
+    before_unbounded = before_unbounded,
+    after_unbounded = after_unbounded,
+    before_positive = before_positive,
+    after_positive = after_positive,
+    step = step,
+    complete = complete,
+    constrain = constrain,
+    iteration = iteration,
+    iteration_max = iteration_max,
+    n = n
+  )
 }
 
 # ------------------------------------------------------------------------------
 
 init_window_params <- function(params) {
-  if (is_unbounded(params$before)) {
+  window_adjustment <- 0L
+
+  # Forward adjustment to match the number of iterations
+  if (params$complete) {
+    if (params$before_positive) {
+      window_adjustment <- params$before
+    }
+  } else {
+    if (!params$after_positive) {
+      window_adjustment <- abs(params$after)
+    }
+  }
+
+  if (params$before_unbounded) {
     start <- 1L
     start_step <- 0L
-    start_step_one <- 0L
-    start_ahead <- FALSE
   } else {
-    start <- 1L - params$before
+    start <- 1L - params$before + window_adjustment
     start_step <- params$step
-    start_step_one <- 1L
-    start_ahead <- params$before < 0L
   }
 
-  if (is_unbounded(params$after)) {
+  if (params$after_unbounded) {
     stop <- params$n
     stop_step <- 0L
-    stop_step_one <- 0L
-    stop_behind <- FALSE
   } else {
-    stop <- 1L + params$after
+    stop <- 1L + params$after + window_adjustment
     stop_step <- params$step
-    stop_step_one <- 1L
-    stop_behind <- params$after < 0L
   }
-
-  first <- 1L
-  last <- params$n
 
   window_params <- list(
     start = start,
     stop = stop,
     start_step = start_step,
-    stop_step = stop_step,
-    start_step_one = start_step_one,
-    stop_step_one = stop_step_one,
-    start_ahead = start_ahead,
-    stop_behind = stop_behind,
-    first = first,
-    last = last
+    stop_step = stop_step
   )
 
-  window_params
-}
-
-# ------------------------------------------------------------------------------
-
-# Conceptually there are 4 ways to get out of bounds, and in these cases no
-# evalution of the function should be made, and the parameters should be
-# incremented by 1, not by the step value.
-
-# 1. Start of the window is past the last data point
-# slide(1:5, ~.x, .before = -1, .after = 1)
-# 1 2 3 4 5 . . .
-# . . . . . | - |
-#           ^
-#           |- Start of window outside range
-
-is_start_ahead_of_last <- function(start, last, start_ahead) {
-  start_ahead && start > last
-}
-
-is_window_start_ahead_of_last <- function(window_params) {
-  is_start_ahead_of_last(window_params$start, window_params$last, window_params$start_ahead)
-}
-
-# 2. End of the window is before the first data point
-# slide(1:5, ~.x, .before = 1, .after = -1)
-# . . . 1 2 3 4 5
-# | - | . . . . .
-#     ^
-#     |- End of window outside range
-
-is_stop_behind_first <- function(stop, first, stop_behind) {
-  stop_behind && stop < first
-}
-
-is_window_stop_behind_first <- function(window_params) {
-  is_stop_behind_first(window_params$stop, window_params$first, window_params$stop_behind)
-}
-
-# 3. Start of the window is before the first data point, and `.complete = TRUE`
-# slide(1:5, ~.x, .before = 1, .complete = TRUE)
-# . 1 2 3 4 5 . .
-# | - | . . . . .
-# ^
-# |- Start of window outside range
-
-is_start_behind_first <- function(start, first) {
-  start < first
-}
-
-is_window_start_behind_first <- function(window_params) {
-  is_start_behind_first(window_params$start, window_params$first)
-}
-
-# 3. End of the window is after the last data point, and `.complete = TRUE`
-# slide(1:5, ~.x, .after = 1, .complete = TRUE)
-# 1 2 3 4 5 . . .
-# . . . | - | . .
-#           ^
-#           |- End of window outside range
-
-is_stop_ahead_of_last <- function(stop, last) {
-  stop > last
-}
-
-is_window_stop_ahead_of_last <- function(window_params) {
-  is_stop_ahead_of_last(window_params$stop, window_params$last)
-}
-
-# ------------------------------------------------------------------------------
-
-loop_bounded_new <- function(x, f, params, window_params, ...) {
-  out <- vec_init(params$ptype, params$n)
-
-  while(params$position <= params$n) {
-    if (params$complete) {
-      if (is_window_start_behind_first(window_params)) {
-        params <- increment_params_by_one(params)
-        window_params <- increment_window_by_one(window_params)
-        next
-      }
-
-      if (is_window_stop_ahead_of_last(window_params)) {
-        params <- increment_params_by_one(params)
-        window_params <- increment_window_by_one(window_params)
-        next
-      }
-    }
-
-    if (is_window_start_ahead_of_last(window_params)) {
-      params <- increment_params_by_one(params)
-      window_params <- increment_window_by_one(window_params)
-      next
-    }
-
-    if (is_window_stop_behind_first(window_params)) {
-      params <- increment_params_by_one(params)
-      window_params <- increment_window_by_one(window_params)
-      next
-    }
-
-    bounded_window_start <- max(window_params$start, 1L)
-    bounded_window_stop <- min(window_params$stop, params$n)
-
-    out <- slice_eval_assign(out, x, f, bounded_window_start, bounded_window_stop, params, ...)
-
-    params <- increment_params_by_step(params)
-    window_params <- increment_window_by_step(window_params)
-  }
-
-  out
-}
-
-loop_before_unbounded_new <- function(x, f, params, window_params, ...) {
-  out <- vec_init(params$ptype, params$n)
-
-  while(params$position <= params$n) {
-    if (params$complete) {
-      if (is_window_stop_ahead_of_last(window_params)) {
-        params <- increment_params_by_one(params)
-        window_params <- increment_window_by_one(window_params)
-        next
-      }
-    }
-
-    if (is_window_stop_behind_first(window_params)) {
-      params <- increment_params_by_one(params)
-      window_params <- increment_window_by_one(window_params)
-      next
-    }
-
-    bounded_window_stop <- min(window_params$stop, params$n)
-
-    out <- slice_eval_assign(out, x, f, window_params$start, bounded_window_stop, params, ...)
-
-    params <- increment_params_by_step(params)
-    window_params <- increment_window_by_step(window_params)
-  }
-
-  out
-}
-
-loop_after_unbounded_new <- function(x, f, params, window_params, ...) {
-  out <- vec_init(params$ptype, params$n)
-
-  while(params$position <= params$n) {
-    if (params$complete) {
-      if (is_window_start_behind_first(window_params)) {
-        params <- increment_params_by_one(params)
-        window_params <- increment_window_by_one(window_params)
-        next
-      }
-    }
-
-    if (is_window_start_ahead_of_last(window_params)) {
-      params <- increment_params_by_one(params)
-      window_params <- increment_window_by_one(window_params)
-      next
-    }
-
-    bounded_window_start <- max(window_params$start, 1L)
-
-    out <- slice_eval_assign(out, x, f, bounded_window_start, window_params$stop, params, ...)
-
-    params <- increment_params_by_step(params)
-    window_params <- increment_window_by_step(window_params)
-  }
-
-  out
-}
-
-loop_double_unbounded_new <- function(x, f, params, window_params, ...) {
-  out <- vec_init(params$ptype, params$n)
-
-  while(params$position <= params$n) {
-    out <- slice_eval_assign(out, x, f, window_params$start, window_params$stop, params, ...)
-
-    params <- increment_params_by_step(params)
-    window_params <- increment_window_by_step(window_params)
-  }
-
-  out
-}
-
-# ------------------------------------------------------------------------------
-
-increment_params_by_one <- function(params) {
-  params$position <- params$position + 1L
-  params$entry <- params$entry + 1L
-  params
-}
-
-increment_params_by_step <- function(params) {
-  params$position <- params$position + params$step
-  params$entry <- params$entry + params$step
-  params
-}
-
-increment_window_by_one <- function(window_params) {
-  window_params$start <- window_params$start + window_params$start_step_one
-  window_params$stop <- window_params$stop + window_params$stop_step_one
-  window_params
-}
-
-increment_window_by_step <- function(window_params) {
-  window_params$start <- window_params$start + window_params$start_step
-  window_params$stop <- window_params$stop + window_params$stop_step
   window_params
 }
 
