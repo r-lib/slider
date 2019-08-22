af062fad75b107fa14c5a3e02d1aa1716d79ee51
DavisVaughan <davis@rstudio.com>
2019-08-22 09:49:30 -0400

    Consolidate impl files

--- R/pslide.R
+++ R/pslide.R
@@ -54,7 +54,8 @@ pslide_vec <- function(.l,
     .after = .after,
     .step = .step,
     .complete = .complete,
-    .ptype = .ptype
+    .ptype = .ptype,
+    .constrain = TRUE
   )
 }
 
@@ -235,4 +236,63 @@ pslide_dfc <- function(.l,
   )
 
   vec_cbind(!!!out, .size = .size, .name_repair = .name_repair)
+}
+
+# ------------------------------------------------------------------------------
+
+pslide_impl <- function(.l,
+                        .f,
+                        ...,
+                        .before,
+                        .after,
+                        .step,
+                        .complete,
+                        .ptype,
+                        .constrain) {
+  if (!is.list(.l)) {
+    abort(paste0("`.l` must be a list, not ", vec_ptype_full(.l), "."))
+  }
+
+  lapply(.l, vec_assert)
+
+  # TODO - Do more efficiently internally by reusing rather than recycling
+  # https://github.com/tidyverse/purrr/blob/e4d553989e3d18692ebeeedb334b6223ae9ea294/src/map.c#L129
+  # But use `vec_size_common()` to check sizes and get `.size`
+  .l <- vec_recycle_common(!!!.l)
+
+  .f <- as_function(.f)
+
+  type <- vec_size(.l)
+
+  slicers <- lapply(
+    seq_len(type),
+    function(.i) {
+      expr(.l[[!!.i]])
+    }
+  )
+
+  # Ensure names of `.l` are kept so they can be spliced
+  # into `.f` as argument names
+  names(slicers) <- names(.l)
+
+  f_call <- expr(.f(!!! slicers, ...))
+
+  params <- list(
+    type,
+    .constrain,
+    .before,
+    .after,
+    .step,
+    .complete
+  )
+
+  out <- slide_core(
+    x = .l,
+    f_call = f_call,
+    ptype = .ptype,
+    env = environment(),
+    params = params
+  )
+
+  out
 }
--- R/slide.R
+++ R/slide.R
@@ -361,3 +361,40 @@ slide_dfc <- function(.x,
 
   vec_cbind(!!!out, .size = .size, .name_repair = .name_repair)
 }
+
+# ------------------------------------------------------------------------------
+
+slide_impl <- function(.x,
+                       .f,
+                       ...,
+                       .before,
+                       .after,
+                       .step,
+                       .complete,
+                       .constrain,
+                       .ptype) {
+  vec_assert(.x)
+
+  .f <- as_function(.f)
+
+  f_call <- expr(.f(.x, ...))
+
+  type <- -1L
+
+  params <- list(
+    type = type,
+    constrain = .constrain,
+    before = .before,
+    after = .after,
+    step = .step,
+    complete = .complete
+  )
+
+  slide_core(
+    x = .x,
+    f_call = f_call,
+    ptype = .ptype,
+    env = environment(),
+    params = params
+  )
+}
