103e2648cfb3ecc2918d3da559de8a6912d5d8a8
DavisVaughan <davis@rstudio.com>
2019-08-22 16:31:00 -0400

    More slide_index2_vec tests

--- tests/testthat/test-slide-index2-vec.R
+++ tests/testthat/test-slide-index2-vec.R
@@ -1,4 +1,28 @@
 # ------------------------------------------------------------------------------
+# slide_index2_vec
+
+test_that("slide_index2_vec() works", {
+  expect_equivalent(slide_index2_vec(1L, 1L, 1, ~.x + .y), list(2L))
+})
+
+test_that("slide_index2_vec() retains names of x", {
+  expect_equivalent(slide_index2_vec(c(x = 1L), c(y = 1L), 1, ~.x + .y), list(x = 2L))
+})
+
+test_that("slide_index2_vec() can simplify automatically", {
+  expect_equivalent(slide_index2_vec(1, 2, 1, ~.x + .y, .ptype = NULL), 3)
+})
+
+test_that("slide_index2_vec() errors if it can't simplify", {
+  fn <- function(x, y) if (x == 1L) {1} else {"hi"}
+  expect_error(
+    slide_index2_vec(1:2, 1:2, 1:2, fn, .ptype = NULL),
+    class = "vctrs_error_incompatible_type"
+  )
+})
+
+
+# ------------------------------------------------------------------------------
 # suffix tests
 
 test_that("slide_index2_int() works", {
