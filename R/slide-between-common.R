d07a602c05e307db0414cc4f3ebc827b69a40b50
DavisVaughan <davis@rstudio.com>
2019-08-25 19:42:52 -0400

    Error message refactoring

--- R/slide-between-common.R
+++ R/slide-between-common.R
@@ -11,23 +11,19 @@ slide_between_common <- function(x,
   x_size <- compute_size(x, type)
 
   check_index_size(x_size, i)
-  check_index_not_na(i)
+  check_not_na(i, "`.i`")
   check_index_ascending(i)
 
-  # TODO - better message
-  check_range_not_na(starts, ".starts")
-  check_range_not_na(stops, ".stops")
+  check_not_na(starts, "`.starts`")
+  check_ascending(starts, "`.starts`")
 
-  # TODO - better message
-  check_ascending(starts, ".starts")
-  check_ascending(stops, ".stops")
+  check_not_na(stops, "`.stops`")
+  check_ascending(stops, "`.stops`")
 
-  out_size <- vec_size(starts)
-  if (out_size != vec_size(stops)) {
-    abort("size of `starts` and `stops` must be identical.")
-  }
+  out_size <- vec_size_common(starts, stops)
 
-  args <- vec_cast_common(i, starts, stops)
+  args <- vec_recycle_common(starts, stops, .size = out_size)
+  args <- vec_cast_common(i, !!!args)
   args <- lapply(args, vec_proxy_compare)
 
   i <- args[[1L]]
