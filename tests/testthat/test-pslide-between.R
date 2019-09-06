21836c45d198669975522fea30e78cec63346042
DavisVaughan <davis@rstudio.com>
2019-09-06 11:48:58 -0400

    Add `pslide_between()`

---
+++ tests/testthat/test-pslide-between.R
@@ -0,0 +1,11 @@
+test_that("empty input returns a list, but after the index size check", {
+  expect_equal(pslide_between(list(integer(), integer()), integer(), integer(), integer(), ~.x), list())
+  expect_equal(pslide_between(list(integer(), 1), integer(), integer(), integer(), ~.x), list())
+  expect_equal(pslide_between(list(1, integer()), integer(), integer(), integer(), ~.x), list())
+
+  expect_error(pslide_between(list(integer(), integer()), 1, integer(), integer(), ~.x), "must be the same")
+})
+
+test_that("completely empty input returns a list", {
+  expect_equal(pslide_between(list(), integer(), integer(), integer(), ~.x), list())
+})
