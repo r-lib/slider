fb74d44f5babe35cd23ba4f2c0c8f69d52121ab5
Davis Vaughan <davis@rstudio.com>
2019-09-24 15:36:27 +0000

    Use short vctrs callables (#27)
    * Update to use `short_*()` callables
    
    * Back to r-lib/vctrs Remote

--- src/slide-vctrs.c
+++ src/slide-vctrs.c
@@ -4,7 +4,11 @@
 SEXP (*vctrs_cast)(SEXP, SEXP, SEXP, SEXP) = NULL;
 SEXP (*compact_seq)(R_len_t, R_len_t, bool) = NULL;
 SEXP (*init_compact_seq)(int*, R_len_t, R_len_t, bool) = NULL;
+
+// `short_*()` callables
+SEXP (*vec_init)(SEXP, R_len_t) = NULL;
 R_len_t (*vec_size)(SEXP) = NULL;
+SEXP (*vec_recycle)(SEXP, R_len_t) = NULL;
 
 void slide_init_vctrs() {
   vctrs_init_api();
@@ -13,5 +17,9 @@ void slide_init_vctrs() {
   vctrs_cast = (SEXP (*)(SEXP, SEXP, SEXP, SEXP)) R_GetCCallable("vctrs", "vctrs_cast");
   compact_seq = (SEXP (*)(R_len_t, R_len_t, bool)) R_GetCCallable("vctrs", "compact_seq");
   init_compact_seq = (SEXP (*)(int*, R_len_t, R_len_t, bool)) R_GetCCallable("vctrs", "init_compact_seq");
-  vec_size = (R_len_t (*)(SEXP)) R_GetCCallable("vctrs", "vec_short_size");
+
+  // `short_*()` callables
+  vec_init = (SEXP (*)(SEXP, R_len_t)) R_GetCCallable("vctrs", "short_vec_init");
+  vec_size = (R_len_t (*)(SEXP)) R_GetCCallable("vctrs", "short_vec_size");
+  vec_recycle = (SEXP (*)(SEXP, R_len_t)) R_GetCCallable("vctrs", "short_vec_recycle");
 }
