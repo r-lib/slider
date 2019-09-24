44448cea950091407c31660fbe4991a2080b2e64
DavisVaughan <davis@rstudio.com>
2019-09-24 10:25:26 -0400

    Update to use `short_*()` callables

--- src/slide-vctrs.h
+++ src/slide-vctrs.h
@@ -7,6 +7,10 @@
 SEXP (*vctrs_cast)(SEXP, SEXP, SEXP, SEXP);
 SEXP (*compact_seq)(R_len_t, R_len_t, bool);
 SEXP (*init_compact_seq)(int*, R_len_t, R_len_t, bool);
+
+// `short_*()` callables
+SEXP (*vec_init)(SEXP, R_len_t);
 R_len_t (*vec_size)(SEXP);
+SEXP (*vec_recycle)(SEXP, R_len_t);
 
 #endif
