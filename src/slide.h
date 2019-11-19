95123a48b6da7f6981227925ae6f239f99074081
DavisVaughan <davis@rstudio.com>
2019-11-19 09:52:59 -0500

    Add compat `_RO` accessors

--- src/slide.h
+++ src/slide.h
@@ -7,8 +7,21 @@
 #include <Rinternals.h>
 #include <stdbool.h>
 
+// Definitions --------------------------------------------------
+
 #define PSLIDE_EMPTY 0
 #define SLIDE -1
 #define SLIDE2 -2
+
+// Compatibility ------------------------------------------------
+
+#if (R_VERSION < R_Version(3, 5, 0))
+  # define LOGICAL_RO(x) ((const int*) LOGICAL(x))
+  # define INTEGER_RO(x) ((const int*) INTEGER(x))
+  # define REAL_RO(x) ((const double*) REAL(x))
+  # define COMPLEX_RO(x) ((const Rcomplex*) COMPLEX(x))
+  # define STRING_PTR_RO(x) ((const SEXP*) STRING_PTR(x))
+  # define RAW_RO(x) ((const Rbyte*) RAW(x))
+#endif
 
 #endif
