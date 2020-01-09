d7f8cf901cd0821e17eef20e6fe5846fe290a17a
DavisVaughan <davis@rstudio.com>
2020-01-08 20:57:22 -0500

    Update to vctrs `vec_as_subscript()`

--- R/hop-common.R
+++ R/hop-common.R
@@ -5,8 +5,8 @@ hop_common <- function(x, starts, stops, f_call, ptype, env, type, constrain) {
   check_not_na(stops, "`.stops`")
 
   # TODO `convert_values = "none"` would be nice to disallow negative positions
-  starts <- vec_coerce_index(starts, allow_types = "position", arg = ".starts")
-  stops <- vec_coerce_index(stops, allow_types = "position", arg = ".stops")
+  starts <- vec_as_subscript(starts, allow_types = "location", arg = ".starts")
+  stops <- vec_as_subscript(stops, allow_types = "location", arg = ".stops")
 
   size <- vec_size_common(starts, stops)
   args <- vec_recycle_common(starts, stops, .size = size)
