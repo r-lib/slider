1263ff19f5be7197f75ab60fa3a24050c39057b6
DavisVaughan <davis@rstudio.com>
2020-01-22 20:34:08 -0500

    Add `slide_period2()` and friends

---
+++ tests/testthat/test-slide-period2.R
@@ -0,0 +1,45 @@
+test_that("empty input returns a list, but after the index size check", {
+  expect_equal(
+    slide_period2(
+      .x = integer(),
+      .y = integer(),
+      .i = structure(numeric(), class = "Date"),
+      .period = "day",
+      .f = ~.x
+    ),
+    list()
+  )
+
+  expect_equal(
+    slide_period2(
+      .x = integer(),
+      .y = 1,
+      .i = structure(numeric(), class = "Date"),
+      .period = "day",
+      .f = ~.x
+    ),
+    list()
+  )
+
+  expect_equal(
+    slide_period2(
+      .x = 1,
+      .y = integer(),
+      .i = structure(numeric(), class = "Date"),
+      .period = "day",
+      .f = ~.x
+    ),
+    list()
+  )
+
+  expect_error(
+    slide_period2(
+      .x = integer(),
+      .y = integer(),
+      .i = structure(0, class = "Date"),
+      .period = "day",
+      .f = ~.x
+    ),
+    class = "slide_error_index_incompatible_size"
+  )
+})
