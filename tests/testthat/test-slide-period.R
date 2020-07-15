9839325c0e3cdefdb6b35df5a8999044a4f27958
DavisVaughan <davis@rstudio.com>
2020-07-15 16:51:50 -0400

    Update tests to use numeric values in date constructor

--- tests/testthat/test-slide-period.R
+++ tests/testthat/test-slide-period.R
@@ -10,11 +10,11 @@ test_that("`.x` must be a vector", {
 })
 
 test_that(".x must be the same size as .i", {
-  expect_error(slide_period(1, new_date(1:2), "year", identity), class = "slider_error_index_incompatible_size")
+  expect_error(slide_period(1, new_date(c(1, 2)), "year", identity), class = "slider_error_index_incompatible_size")
 })
 
 test_that(".i must be ascending", {
-  expect_error(slide_period(1:2, new_date(2:1), "year", identity), class = "slider_error_index_must_be_ascending")
+  expect_error(slide_period(1:2, new_date(c(2, 1)), "year", identity), class = "slider_error_index_must_be_ascending")
 })
 
 test_that("empty input returns a list, but after the index size check", {
