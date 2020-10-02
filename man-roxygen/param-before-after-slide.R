affdf58e6aa4fc3ce9580d2c2aca995832fa85e2
DavisVaughan <davis@rstudio.com>
2020-10-02 11:53:48 -0400

    One more step to revert `.after` doc changes

--- man-roxygen/param-before-after-slide.R
+++ man-roxygen/param-before-after-slide.R
@@ -4,4 +4,4 @@
 #'   include in the sliding window. Set to `Inf` to select all elements
 #'   before or after the current element. Negative values are allowed, which
 #'   allows you to "look forward" from the current element if used as the
-#'   `.before` value, or "look backwards" if used as the `.after` value.
+#'   `.before` value, or "look backwards" if used as `.after`.
