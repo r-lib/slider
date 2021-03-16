b0543a36c5ac218e6cd44c5d74c0a90d41ab7fab
DavisVaughan <davis@rstudio.com>
2021-03-16 11:52:57 -0400

    Initiate update to testthat 3.0.0

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library(testthat)
 library(slider)
 
-if (requireNamespace("xml2")) {
-  test_check("slider", reporter = MultiReporter$new(reporters = list(JunitReporter$new(file = "test-results.xml"), CheckReporter$new())))
-} else {
-  test_check("slider")
-}
+test_check("slider")
