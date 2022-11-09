095a0f910afe01573280582e7f769d904a4162c9
Davis Vaughan <davis@rstudio.com>
2022-11-09 13:21:53 -0500

    Remove wrappers of some now exported vctrs functions (#181)

--- src/slider-vctrs-private.c
+++ src/slider-vctrs-private.c
@@ -5,7 +5,6 @@ SEXP (*vec_cast)(SEXP, SEXP) = NULL;
 SEXP (*vec_chop)(SEXP, SEXP) = NULL;
 SEXP (*vec_slice_impl)(SEXP, SEXP) = NULL;
 SEXP (*vec_names)(SEXP) = NULL;
-SEXP (*vec_set_names)(SEXP, SEXP) = NULL;
 SEXP (*compact_seq)(R_len_t, R_len_t, bool) = NULL;
 SEXP (*init_compact_seq)(int*, R_len_t, R_len_t, bool) = NULL;
 
@@ -15,7 +14,6 @@ void slider_initialize_vctrs_private(void) {
   vec_chop = (SEXP (*)(SEXP, SEXP)) R_GetCCallable("vctrs", "exp_vec_chop");
   vec_slice_impl = (SEXP (*)(SEXP, SEXP)) R_GetCCallable("vctrs", "exp_vec_slice_impl");
   vec_names = (SEXP (*)(SEXP)) R_GetCCallable("vctrs", "exp_vec_names");
-  vec_set_names = (SEXP (*)(SEXP, SEXP)) R_GetCCallable("vctrs", "exp_vec_set_names");
   compact_seq = (SEXP (*)(R_len_t, R_len_t, bool)) R_GetCCallable("vctrs", "exp_short_compact_seq");
   init_compact_seq = (SEXP (*)(int*, R_len_t, R_len_t, bool)) R_GetCCallable("vctrs", "exp_short_init_compact_seq");
 }
--- src/slider-vctrs-private.h
+++ src/slider-vctrs-private.h
@@ -8,7 +8,6 @@ extern SEXP (*vec_cast)(SEXP, SEXP);
 extern SEXP (*vec_chop)(SEXP, SEXP);
 extern SEXP (*vec_slice_impl)(SEXP, SEXP);
 extern SEXP (*vec_names)(SEXP);
-extern SEXP (*vec_set_names)(SEXP, SEXP);
 extern SEXP (*compact_seq)(R_len_t, R_len_t, bool);
 extern SEXP (*init_compact_seq)(int*, R_len_t, R_len_t, bool);
 
